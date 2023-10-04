6254e842680fd725ac64d3bf055106a707e773c1
lucaslija <81848948+lucaslija@users.noreply.github.com>
2023-10-03 21:05:23 -0700

    Added buttonGen()

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ let firstNum;
 let secondNum;
 let operator;
 
+const display = document.querySelector("#display");
+const buttons = document.querySelector("#buttons");
+
 // Operators
 function add(a, b) {
     return a + b;
@@ -29,5 +32,14 @@ function operate(operator, a, b) {
         multiply(a, b);
     } else {
         divide(a, b);
+    }
+}
+
+// Display
+function buttonGen() {
+    for (i = 0; i < 20; i++) {
+        let newButton = document.createElement("div");
+        newButton.className = "btn";
+        buttons.appendChild(newButton);
     }
 }
